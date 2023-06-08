d43b00c1227781185e904b4485654f9b6949d0dd
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-06-08 18:58:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import Tasklist from '../modules/tasklist.js';
-import addItem from '../modules/addItem.js';
-import displayItem from '../modules/displayItem.js';
 
-class App {
-  constructor() {
-    this.myTasklist = new Tasklist();
-    this.myTasklist.addBtn.addEventListener('click', this.myAddItem.bind(this));
-  }
+const arrayOfTasks = JSON.parse(localStorage.getItem('arrayOfTasks')) || [
+  { description: ' Cleaning', completed: false, index: 1 },
+  { description: 'Reading', completed: false, index: 2 },
+  { description: 'Walking', completed: false, index: 3 },
+];
+const toDoList = document.querySelector('.toDoList');
 
-  myDisplayItem() {
-    displayItem(this.myTasklist.listOfTasks);
-  }
+const displayItem = () => {
+  arrayOfTasks.forEach((todo) => {
+    const div = document.createElement('div');
+    div.classList.add('listItem');
+    const attr = document.createAttribute('data-id');
+    attr.value = todo.index;
+    div.setAttributeNode(attr);
 
-  myAddItem() {
-    addItem(this.myTasklist.listOfTasks);
-  }
-}
+    const divElement = document.createElement('div');
+    divElement.className = 'checkText';
+    const inputElement = document.createElement('input');
+    inputElement.type = 'checkbox';
+    inputElement.id = 'myCheckbox';
+    const inputTextElement = document.createElement('input');
+    inputTextElement.classList.add('toDoText');
+    inputTextElement.type = 'text';
+    inputTextElement.value = todo.description;
+    divElement.appendChild(inputElement);
+    divElement.appendChild(inputTextElement);
+    const buttonElement = document.createElement('button');
+    buttonElement.type = 'button';
+    buttonElement.className = 'btn';
+    const iElement = document.createElement('i');
+    iElement.className = 'fa-solid fa-ellipsis-vertical';
+    buttonElement.appendChild(iElement);
+    div.appendChild(divElement);
+    div.appendChild(buttonElement);
+    toDoList.appendChild(div);
+  });
+  localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
+};
 
-const app = new App();
-app.myDisplayItem();
+document.addEventListener('DOMContentLoaded', displayItem);

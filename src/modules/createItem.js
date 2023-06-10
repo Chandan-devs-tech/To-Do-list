9f483cc8d7a2e36f9bd5ef5b224d869b3b29ec5e
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-06-10 22:30:42 +0530

    Modify some changes

--- src/modules/createItem.js
+++ src/modules/createItem.js
@@ -1,54 +1,54 @@
 // eslint-disable-next-line
 import { arrayOfTasks, clearItem, deleteTask } from "./addItem.js";
-import checker from "./checker.js";
-import createIcon from "./createIcon.js";
+import checker from './checker.js';
+import createIcon from './createIcon.js';
 
-const clearAllBtn = document.querySelector(".clearList");
+const clearAllBtn = document.querySelector('.clearList');
 
 // createElement function
 const createElement = (todo) => {
-  const div = document.createElement("div");
-  div.classList.add("listItem");
-  const attr = document.createAttribute("data-id");
+  const div = document.createElement('div');
+  div.classList.add('listItem');
+  const attr = document.createAttribute('data-id');
   attr.value = todo.index;
   div.setAttributeNode(attr);
   //   creating div for checkbox and input
-  const divElement = document.createElement("div");
-  divElement.className = "checkText";
+  const divElement = document.createElement('div');
+  divElement.className = 'checkText';
   //   creating text input
-  const inputTextElement = document.createElement("input");
-  inputTextElement.classList.add("toDoText");
-  inputTextElement.type = "text";
+  const inputTextElement = document.createElement('input');
+  inputTextElement.classList.add('toDoText');
+  inputTextElement.type = 'text';
   inputTextElement.value = todo.description;
   // event listener for edit
-  inputTextElement.addEventListener("blur", () => {
+  inputTextElement.addEventListener('blur', () => {
     const newInputValue = inputTextElement.value;
     todo.description = newInputValue;
-    localStorage.setItem("arrayOfTasks", JSON.stringify(arrayOfTasks));
+    localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
   });
   // creating checkbox
-  const inputElement = document.createElement("input");
-  inputElement.type = "checkbox";
-  inputElement.id = "myCheckbox";
+  const inputElement = document.createElement('input');
+  inputElement.type = 'checkbox';
+  inputElement.id = 'myCheckbox';
   //   checkbox event listener
-  inputElement.addEventListener("change", () => {
+  inputElement.addEventListener('change', () => {
     checker(inputTextElement, inputElement, div, todo, arrayOfTasks);
     // localStorage.setItem("arrayOfTasks", JSON.stringify(arrayOfTasks));
   });
   if (todo.completed) {
-    inputTextElement.classList.toggle("lineThrough");
-    div.classList.toggle("completed");
+    inputTextElement.classList.toggle('lineThrough');
+    div.classList.toggle('completed');
     inputElement.checked = true;
   }
   //    clear all event listener
-  clearAllBtn.addEventListener("click", clearItem);
+  clearAllBtn.addEventListener('click', clearItem);
   //   appending checkbox and text input to the div
   divElement.appendChild(inputElement);
   divElement.appendChild(inputTextElement);
   // create icon function called
-  const icon = createIcon("fa-trash-can");
+  const icon = createIcon('fa-trash-can');
   // add event listener delete icon
-  icon.addEventListener("click", () => deleteTask(todo.index));
+  icon.addEventListener('click', () => deleteTask(todo.index));
   //   buttonElement.appendChild(iElement);
   div.appendChild(divElement);
   div.appendChild(icon);

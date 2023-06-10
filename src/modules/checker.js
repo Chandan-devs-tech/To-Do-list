1a7be6fd039b503d20018077281b48feace27b0f
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-06-10 22:29:11 +0530

    Modify some changes

--- src/modules/checker.js
+++ src/modules/checker.js
@@ -1,14 +1,15 @@
 // checker function
-const checker = (input, checkbox, element, task) => {
+const checker = (input, checkbox, element, task, array) => {
   if (checkbox.checked) {
-    input.classList.toggle('lineThrough');
-    element.classList.toggle('completed');
+    input.classList.toggle("lineThrough");
+    element.classList.toggle("completed");
     task.completed = true;
   } else {
-    input.classList.toggle('lineThrough');
-    element.classList.toggle('completed');
+    input.classList.toggle("lineThrough");
+    element.classList.toggle("completed");
     task.completed = false;
   }
+  localStorage.setItem("arrayOfTasks", JSON.stringify(array));
 };
 
 export default checker;
